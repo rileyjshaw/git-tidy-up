901b7095efa3439002e343c064153cf331c7d020
phil <uniphil@gmail.com>
2016-10-17 23:42:31 -0400

    Make it python3-compatible
    - print is a function (a __future__ import to keep it sane in py2)
    - ever-thrilling encoding excitement to get a string back from subprocess
    - the beauty of py3 reclaiming `input` for 2's `raw_input`.

--- git-tidy-up.py
+++ git-tidy-up.py
@@ -1,7 +1,13 @@
 #!/usr/bin/env python
 
+from __future__ import print_function
 import re
 import subprocess
+
+try:
+    input = raw_input  # fix for python2
+except NameError:
+    pass  # no raw_input in py3, but input is already what we want
 
 
 process = subprocess.Popen(['git', 'diff', '-U0'], stdout=subprocess.PIPE)
@@ -9,23 +15,23 @@ output, err = process.communicate()
 
 # For each modified file's patch...
 # TODO(riley): I am bad at programming lol help.
-for f in re.findall(re.compile('^(diff.*?)(?=^diff|\Z)', re.M | re.S), output):
+for f in re.findall(re.compile('^(diff.*?)(?=^diff|\Z)', re.M | re.S), output.decode()):
     sections = re.split(re.compile('^@@', re.M), f)
     header = sections[0]
     diffs = ['@@' + s for s in sections[1:]]
     for diff in diffs:
         patch = header + '\n' + diff
-        print patch
+        print(patch)
         while True:
-            c = raw_input('\033[1m\033[94mWhat should I do [[1-9],y,n]?\033[0m ')
+            c = input('\033[1m\033[94mWhat should I do [[1-9],y,n]?\033[0m ')
             if len(c) is 1 and c.isdigit() and c != '0':
-                print 'Let\'s commit that to batch ' + c + '.'
+                print('Let\'s commit that to batch ' + c + '.')
             elif c == 'y':
-                print 'Okay, we will skip it.'
+                print('Okay, we will skip it.')
             elif c == 'n':
-                print 'Hoo boy, deleted.'
+                print('Hoo boy, deleted.')
             else:
                 continue
 
-            print '\n'
+            print('\n')
             break
