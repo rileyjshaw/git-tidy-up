6682d0a8249a5ef456ba25f40a1c7a8d78a2341b
phil <uniphil@gmail.com>
2016-10-17 23:56:15 -0400

    Make exiting friendlier
    `q` added as a normal input, which immitates ctrl+C and breaks out of the loop
    
    KeyboardInterrupt is caught (from `q` or ctrl+C) and just passed.
    
    It just exits quietly. Maybe we should print a poem.

--- git-tidy-up.py
+++ git-tidy-up.py
@@ -15,23 +15,28 @@ output, err = process.communicate()
 
 # For each modified file's patch...
 # TODO(riley): I am bad at programming lol help.
-for f in re.findall(re.compile('^(diff.*?)(?=^diff|\Z)', re.M | re.S), output.decode()):
-    sections = re.split(re.compile('^@@', re.M), f)
-    header = sections[0]
-    diffs = ['@@' + s for s in sections[1:]]
-    for diff in diffs:
-        patch = header + '\n' + diff
-        print(patch)
-        while True:
-            c = input('\033[1m\033[94mWhat should I do [[1-9],y,n]?\033[0m ')
-            if len(c) is 1 and c.isdigit() and c != '0':
-                print('Let\'s commit that to batch ' + c + '.')
-            elif c == 'y':
-                print('Okay, we will skip it.')
-            elif c == 'n':
-                print('Hoo boy, deleted.')
-            else:
-                continue
+try:
+    for f in re.findall(re.compile('^(diff.*?)(?=^diff|\Z)', re.M | re.S), output.decode()):
+        sections = re.split(re.compile('^@@', re.M), f)
+        header = sections[0]
+        diffs = ['@@' + s for s in sections[1:]]
+        for diff in diffs:
+            patch = header + '\n' + diff
+            print(patch)
+            while True:
+                c = input('\033[1m\033[94mWhat should I do [[1-9],y,n,q]?\033[0m ')
+                if len(c) is 1 and c.isdigit() and c != '0':
+                    print('Let\'s commit that to batch ' + c + '.')
+                elif c == 'y':
+                    print('Okay, we will skip it.')
+                elif c == 'n':
+                    print('Hoo boy, deleted.')
+                elif c == 'q':
+                    raise KeyboardInterrupt
+                else:
+                    continue
 
-            print('\n')
-            break
+                print('\n')
+                break
+except KeyboardInterrupt:
+    pass
